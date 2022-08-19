7b1a1cf0c867528895de0b2e7c5fc83ea28d34c8
Ahmad Bilal Ameen <iambilalameen@gmail.com>
2022-08-19 09:15:12 +0500

    Models and Routes Added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return response()->json(['data'=>'test']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUserRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreUserRequest $request)
+    {
+        return response()->json(['data'=> 'posted']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(User $user)
+    {
+        return response()->json(['data'=> $user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUserRequest  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        return response()->json(['data'=>'update']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(User $user)
+    {
+        return response()->json(['data'=> 'deleted successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['prefix'=>'v1'], function(){
+    require __DIR__. '/api/v1/User.php';
+} );
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
